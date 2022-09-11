e29cc3ec05eb411c9f55e046de144d646f5cd20f
SSpoilt <65760879+SSpoilt@users.noreply.github.com>
2022-09-11 23:04:29 +0530

    Add files via upload

---
+++ php form/connect1.php
@@ -0,0 +1,19 @@
+<?php
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$conn = new mysqli('localhost','root','','test');
+if($conn->connect_error){
+  echo "$conn->connect_error";
+  die("Connection Failed : ". $conn->connect_error);
+} else {
+  $stmt = $conn->prepare( " SELECT * FROM registration WHERE email = '$email' && password = '$password' limit 1 ");
+  $execval = $stmt->execute();
+  echo $execval;
+  echo "login successfully...";
+  $stmt->close();
+  $conn->close();
+}
+?>
